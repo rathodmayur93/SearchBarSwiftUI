cef04e89eef668b58a4c825b1029e808d1b273ba
ds-mayur <mayur.rathod@devjisystems.com>
2019-10-05 00:43:22 +0300

    Initial Commit

---
+++ SearchBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SearchBar
+//
+//  Created by ds-mayur on 10/5/19.
+//  Copyright © 2019 Mayur Rathod. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
